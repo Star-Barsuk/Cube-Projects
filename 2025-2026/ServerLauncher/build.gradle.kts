45f72ea56bf59a13a8068a13741b7be024cd2ee7
Star-Barsuk <120922249+Star-Barsuk@users.noreply.github.com>
2026-05-10 00:54:27 +0700

    09.05.26

---
+++ 2025-2026/ServerLauncher/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    java
+    application
+    id("org.javamodularity.moduleplugin") version "1.8.15"
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.beryx.jlink") version "3.2.1"
+}
+
+group = "org.min"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val junitVersion = "5.12.1"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "org.min.app.Launcher",
+            "Implementation-Title" to "MCSL",
+            "Implementation-Version" to project.version
+        )
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+application {
+    mainModule.set("org.min.javafxcoding")
+    mainClass.set("org.min.app.Launcher")
+}
+
+javafx {
+    version = "21.0.6"
+    modules = listOf("javafx.controls", "javafx.fxml", "javafx.graphics")
+}
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.20")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
